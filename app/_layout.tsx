aa1f0cda3bb3941701a842e1ee6f1cac3877f388
Leonardo Perez <A01665462@tec.mx>
2026-05-28 20:13:27 -0600

    chore: init gluestack ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
+    
+    <GluestackUIProvider mode="dark">
+      <QueryClientProvider client={queryClient}>
       <Stack />
     </QueryClientProvider>
+    </GluestackUIProvider>
+  
   );
 }
